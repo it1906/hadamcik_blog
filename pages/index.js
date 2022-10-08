7b4efa57502b1c35a175aebde374e595a9a39ed1
it1906 <githublmao@seznam.cz>
2022-10-08 13:58:44 +0200

    03: Data fetching part 1

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import Head from 'next/head';
 import { PostCard, PostWidget, Categories } from '../components';
+import {getPosts} from '../services';
 
-const posts = [
-  {title: 'What is a calorie deficit', excerpt: 'calorie what? ive never heard of that'},
-  {title: 'What is a calorie deficit test', excerpt: 'youre probably wondering what is a calorie deficit '},
-]
 
-export default function Home() {
+
+export default function Home({posts}) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -15,7 +13,7 @@ export default function Home() {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post, index)=> <PostCard post={post} key={post.title}/>
+          {posts.map((post)=> <PostCard post={post.node} key={post.title}/>
         )}
         </div>
         <div className='lg:col-span-4 col-span-1'>
@@ -29,4 +27,9 @@ export default function Home() {
   )
 }
 
-
+export async function getStaticProps(){
+  const posts = (await getPosts()) || [];
+  return {
+    props: {posts}
+  }
+}
--- services/index.js
+++ services/index.js
@@ -1,33 +1,38 @@
 import {request, gql} from 'graphql-request';
 
-export const getPosts = async () =>{
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export const getPosts = async () => {
     const query = gql `
         query MyQuery {
             postsConnection {
             edges {
                 node {
-                author {
-                    bio
-                    name
-                    id
-                    photo {
-                    url
+                    author {
+                        bio
+                        name
+                        id
+                        photo {
+                        url
+                        }
                     }
-                }
-                createdAt
-                slug
-                title
-                exerpt
-                featuredImage {
-                    url
-                }
-                categories {
-                    name
+                    createdAt
                     slug
-                }
+                    title
+                    exerpt
+                    featuredImage {
+                        url
+                    }
+                    categories {
+                        name
+                        slug
+                    }
                 }
             }
             }
         }
     `
+    const result = await request(graphqlAPI, query);
+
+    return result.postsConnection.edges;
 }
