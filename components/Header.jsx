6f90d32f7e77247f0a8872f2fba436462f52e3f7
it1906 <githublmao@seznam.cz>
2022-10-09 16:12:51 +0200

    05: Data fetching part 3

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,31 +1,40 @@
-import React, {useContext} from 'react'
 import Link from 'next/link'
+import React, { useState, useEffect } from 'react'
+import { getCategories } from '../services'
 
-const categories = [{name: 'Food', slug: 'yum'}, {name: 'Lifestyle', slug:'life'}];
+
+
 
 const Header = () => {
-  return (
-    <div className='container mx-auto px-10 mb-8'>
-        <div className='border-b w-full inline-block border-blue-400 py-8'>
-            <div className='md:float-left block'>
-                <Link href="/">
-                    <span className='cursor-pointer font-bold text-4xl text-white'>
-                        BlogName
-                    </span>
-                </Link>
-            </div>
-            <div className='hidden md:float-left md:contents'>
-                {categories.map((category)=>(
-                    <Link key={category.slug} href={`/category/${category.slug}`}>
-                        <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
-                            {category.name}
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        getCategories()
+            .then((newCategories) => setCategories(newCategories))
+    }, []);
+    
+    return (
+        <div className='container mx-auto px-10 mb-8'>
+            <div className='border-b w-full inline-block border-blue-400 py-8'>
+                <div className='md:float-left block'>
+                    <Link href="/">
+                        <span className='cursor-pointer font-bold text-4xl text-white'>
+                            BlogName
                         </span>
                     </Link>
-                ))}
+                </div>
+                <div className='hidden md:float-left md:contents'>
+                    {categories.map((category) => (
+                        <Link key={category.slug} href={`/category/${category.slug}`}>
+                            <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
+                                {category.name}
+                            </span>
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header
--- services/index.js
+++ services/index.js
@@ -1,9 +1,9 @@
-import {request, gql} from 'graphql-request';
+import { request, gql } from 'graphql-request';
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
 export const getPosts = async () => {
-    const query = gql `
+    const query = gql`
         query MyQuery {
             postsConnection {
             edges {
@@ -35,4 +35,58 @@ export const getPosts = async () => {
     const result = await request(graphqlAPI, query);
 
     return result.postsConnection.edges;
+}
+
+export const getRecentPosts = async () => {
+    const query = gql`
+    query GetPostDetails(){
+        posts(
+            orderBy: createdAt_ASC
+            last: 3
+        ){
+            title
+            featuredImage{
+                url
+            }
+            createdAt
+            slug
+        }
+    }
+    `
+    const result = await request(graphqlAPI, query);
+
+    return result.posts;
+}
+export const getSimilarPosts = async () => {
+    const query = gql`
+    querry GetPostDetails($slug: String!, $categories: [String!]){
+        posts(
+            where: {slug_not: $slug, AND: {categories_some: {slug_in:$categories}}}
+            last: 3
+        ){
+            title
+            featuredImage{
+                url
+            }
+            createdAt
+            slug
+        }
+    }
+    `
+    const result = await request(graphqlAPI, query);
+
+    return result.posts;
+}
+export const getCategories = async () => {
+    const query = gql`
+        query GetCategories{
+            categories {
+                name
+                slug
+            }
+        }
+    `
+    const result = await request(graphqlAPI, query);
+
+    return result.categories;
 }
